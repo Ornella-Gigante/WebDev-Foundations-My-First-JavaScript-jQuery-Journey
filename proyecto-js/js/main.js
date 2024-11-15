02a22c854adf94f2513674e164ac29825d94aea9
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2024-11-15 19:38:30 +0100

    Feature: slider in the webiste

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -1,2 +1,14 @@
 'use strict'
 
+/**
+ * Slider
+ * Pagina usada: https://bxslider.com/examples/image-slideshow-captions
+ */
+$(document).ready(function(){
+    $('.galeria').bxSlider({
+        mode: 'fade',
+        captions: false,
+        responsive: true,
+        pager:true
+      });
+});
