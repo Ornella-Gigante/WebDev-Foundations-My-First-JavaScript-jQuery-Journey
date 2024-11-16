1c51adbca9cd88bfe03ae6fbd19957caa3fc7afb
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2024-11-16 01:01:26 +0100

    Correction cambiador de temas del sitio web

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -147,6 +147,30 @@ posts.forEach((item,index) => {
 });
 
 
+// Selector de tema 
+
+var theme = $("#theme");
+
+$("#to-green").click(function(){
+
+    theme.attr("href", "css/green.css");
+
+});
+
+$("#to-red").click(function(){
+
+    theme.attr("href", "css/red.css");
+
+});
+
+$("#to-blue").click(function(){
+
+    theme.attr("href", "css/blue.css");
+
+});
+
+
+
 
 
 
